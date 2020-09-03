f0a18902ee7f8e878c43bd61f3c59d390c06b120
omarnabi1 <onabi20@gmail.com>
2020-09-03 01:34:08 -0400

    Done.

---
+++ db/migrate/20200903053004_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+      create_table :users do |f|
+          f.string :name
+          f.string :email
+          f.string :image
+          f.string :uid
+      end
+  end
+end    
